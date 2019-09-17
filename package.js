9c846a059480186ff11da5ec70abb24957a9bda1
Emmanuel <egarcia@womgp.com>
2019-09-17 12:07:29 -0500

    primer commit

---
+++ package.js
@@ -0,0 +1,19 @@
+  
+{
+  "name": "paydek",
+  "version": "1.0.0",
+  "description": "server exportaciones",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.16.4"
+  },
+  "devDependencies": {
+    "nodemon": "^1.18.9"
+  }
+}
